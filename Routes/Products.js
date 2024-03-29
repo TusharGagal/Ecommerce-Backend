ce35dadd145403e07581b8c2db50c9ef078ead4a
Tushar Gagal <tushargagal1209@gmail.com>
2024-03-29 12:56:13 +0530

    first commit

---
+++ Routes/Products.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const {
+  createProduct,
+  fetchAllProducts,
+  fetchProductById,
+  updateProduct,
+} = require("../Controller/Product");
+const router = express.Router();
+
+router
+  .post("/", createProduct)
+  .get("/", fetchAllProducts)
+  .get("/:id", fetchProductById)
+  .patch("/:id", updateProduct);
+
+exports.router = router;
