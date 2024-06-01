dc29efc2bf99f0f26ba2967dad5391ba31dad21d
Tushar Gagal <tushargagal1209@gmail.com>
2024-06-01 21:15:02 +0530

    last commit

--- Routes/Products.js
+++ Routes/Products.js
@@ -12,17 +12,6 @@ router
   .post("/", createProduct)
   .get("/", fetchAllProducts)
   .get("/:id", fetchProductById)
-  .patch("/:id", updateProduct)
-  .get("/update/test", async (req, res) => {
-    const products = await Product.find({});
-    for (let product of products) {
-      product.discountedPrice = Math.round(
-        product.price * (1 - product.discountPercentage / 100) * 83
-      );
-      await product.save();
-      console.log(product.title + "updated");
-    }
-    res.send("ok");
-  });
+  .patch("/:id", updateProduct);
 
 exports.router = router;
